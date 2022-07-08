f49804ede3a33120b54d8cf94db3e56f606964ff
favioreinaldo <favioreinaldo@gmail.com>
2022-07-07 21:38:44 -0300

    Add solution to excercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if(num<0){
+        result = 'ERROR';
+    }else{
+        for (let index = 0; index < num; index++) {
+            result += string;
+        }        
+    }
+    return result;
 };
 
 // Do not edit below this line
