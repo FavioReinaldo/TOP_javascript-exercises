75ca46afc1e3e0836046e4aa32070517e91f3e8e
favioreinaldo <favioreinaldo@gmail.com>
2022-07-11 11:47:08 -0300

    add solution excercise 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let resultado = [];
+    let basedArray = args[0];
+    basedArray.forEach(element => {
+        if (!args.includes(element)) {
+            resultado.push(element);
+        }  
+    });
+    return resultado;
+    /*
+    const array = args[0]
+    return array.filter(val => !args.includes(val)) 
+    */
 };
 
 // Do not edit below this line
