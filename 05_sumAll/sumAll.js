d20e16e99a6164a79a0c1416b8e41c3a7481ba05
favioreinaldo <favioreinaldo@gmail.com>
2022-07-11 12:31:23 -0300

    add solution excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(initialNum, finalNum) {
+     let result = 0;
+    if (initialNum < 0 || !Number.isInteger(finalNum)) {
+        return 'ERROR';
+    } else {
+        if (initialNum>finalNum) {
+            let aux = initialNum;
+            initialNum = finalNum;
+            finalNum = aux;
+        }
+        while (initialNum <= finalNum) {
+            result = result + initialNum;
+            initialNum += 1;
+        }
+        return result;
+    }
+    
 };
 
 // Do not edit below this line
